7b4a8291541fe01c355d85c0938ba596912f69ae
24521790- Tra Trong Tín <24521790@gm.uit.edu.vn>
2026-05-10 15:04:54 +0700

    fix lại gộp code vào piece.cpp

--- Piece.cpp
+++ Piece.cpp
@@ -47,3 +47,90 @@ void PieceI::rotate() {
         copyShape(s);
     }
 }
+
+
+// Constructor khởi tạo khối O
+PieceO::PieceO() {
+    char s[4][4] = {
+        {' ', ' ', ' ', ' '},
+        {' ', 'O', 'O', ' '},
+        {' ', 'O', 'O', ' '},
+        {' ', ' ', ' ', ' '}
+    };
+    copyShape(s);
+    state = 0;
+}
+
+// Khối vuông xoay không đổi hình dạng
+void PieceO::rotate() {
+    state = (state + 1) % 4; // Vẫn cập nhật state cho đúng chuẩn nhưng không cần đổi shape
+}
+
+
+// Constructor khởi tạo khối T (Mặc định trạng thái 0)
+PieceT::PieceT() {
+    char s[4][4] = {
+        {' ', 'T', ' ', ' '},
+        {'T', 'T', 'T', ' '},
+        {' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' '}
+    };
+    copyShape(s);
+    state = 0;
+}
+
+// Hàm xoay khối T qua 4 góc
+void PieceT::rotate() {
+    state = (state + 1) % 4;
+
+    if (state == 0) {
+        // T hướng lên: 
+        //   . T .
+        //   T T T
+        char s[4][4] = {
+            {' ', 'T', ' ', ' '},
+            {'T', 'T', 'T', ' '},
+            {' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' '}
+        };
+        copyShape(s);
+    }
+    else if (state == 1) {
+        // T hướng sang phải:
+        //   T . .
+        //   T T .
+        //   T . .
+        char s[4][4] = {
+            {'T', ' ', ' ', ' '},
+            {'T', 'T', ' ', ' '},
+            {'T', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' '}
+        };
+        copyShape(s);
+    }
+    else if (state == 2) {
+        // T hướng xuống:
+        //   T T T
+        //   . T .
+        char s[4][4] = {
+            {'T', 'T', 'T', ' '},
+            {' ', 'T', ' ', ' '},
+            {' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' '}
+        };
+        copyShape(s);
+    }
+    else if (state == 3) {
+        // T hướng sang trái:
+        //   . T .
+        //   T T .
+        //   . T .
+        char s[4][4] = {
+            {' ', 'T', ' ', ' '},
+            {'T', 'T', ' ', ' '},
+            {' ', 'T', ' ', ' '},
+            {' ', ' ', ' ', ' '}
+        };
+        copyShape(s);
+    }
+}
