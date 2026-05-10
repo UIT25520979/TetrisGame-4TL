775f3810ced7e8630a622d69c2e9883c863a5cab
24521790- Tra Trong Tín <24521790@gm.uit.edu.vn>
2026-05-10 14:49:26 +0700

    xong 2 class logic PieceO va PieceT, tui chia ra 2 file cpp Pierce0 vaf Piecre T

---
+++ PieceO.cpp
@@ -0,0 +1,18 @@
+﻿#include "Piece.h"
+
+// Constructor khởi tạo khối O
+PieceO::PieceO() {
+    char s[4][4] = {
+        {' ', ' ', ' ', ' '},
+        {' ', 'O', 'O', ' '},
+        {' ', 'O', 'O', ' '},
+        {' ', ' ', ' ', ' '}
+    };
+    copyShape(s);
+    state = 0;
+}
+
+// Khối vuông xoay không đổi hình dạng
+void PieceO::rotate() {
+    state = (state + 1) % 4; // Vẫn cập nhật state cho đúng chuẩn nhưng không cần đổi shape
+}
