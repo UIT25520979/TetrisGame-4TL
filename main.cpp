102d0ac6531700f1a27c9742b204845e8ae41b19
25521626-NguyenTuanTai <25521626@gm.uit.edu.vn>
2026-05-10 18:59:10 +0700

    Thêm hàm spawmPiece và rotateBlock cho main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <windows.h>
 #include <time.h>
+#include "Piece.h"
 
 using namespace std;
 
@@ -10,86 +11,8 @@ using namespace std;
 #define B_BORDER '#'
 
 char board[H][W] = {};
-char blocks[19][4][4] = {
-        {{' ','I',' ',' '},
-         {' ','I',' ',' '},
-         {' ','I',' ',' '},
-         {' ','I',' ',' '}},
-        {{' ',' ',' ',' '},
-         {'I','I','I','I'},
-         {' ',' ',' ',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','O','O',' '},
-         {' ','O','O',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','T',' ',' '},
-         {'T','T','T',' '},
-         {' ',' ',' ',' '}},
-        {{' ','T',' ',' '},
-         {' ','T','T',' '},
-         {' ','T',' ',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {'T','T','T',' '},
-         {' ','T',' ',' '},
-         {' ',' ',' ',' '}},
-        {{' ','T',' ',' '},
-         {'T','T',' ',' '},
-         {' ','T',' ',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ','S','S',' '},
-         {'S','S',' ',' '},
-         {' ',' ',' ',' '}},
-        {{'S',' ',' ',' '},
-         {'S','S',' ',' '},
-         {' ','S',' ',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {'Z','Z',' ',' '},
-         {' ','Z','Z',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ','Z',' '},
-         {' ','Z','Z',' '},
-         {' ','Z',' ',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {' ',' ','L',' '},
-         {'L','L','L',' '},
-         {' ',' ',' ',' '}},
-        {{'L',' ',' ',' '},
-         {'L',' ',' ',' '},
-         {'L','L',' ',' '},
-         {' ',' ',' ',' '}},
-        {{'L','L','L',' '},
-         {'L',' ',' ',' '},
-         {' ',' ',' ',' '},
-         {' ',' ',' ',' '}},
-        {{' ','L','L',' '},
-         {' ',' ','L',' '},
-         {' ',' ','L',' '},
-         {' ',' ',' ',' '}},
-        {{' ',' ',' ',' '},
-         {'J',' ',' ',' '},
-         {'J','J','J',' '},
-         {' ',' ',' ',' '}},
-        {{' ','J','J',' '},
-         {' ','J',' ',' '},
-         {' ','J',' ',' '},
-         {' ',' ',' ',' '}},
-        {{'J','J','J',' '},
-         {' ',' ','J',' '},
-         {' ',' ',' ',' '},
-         {' ',' ',' ',' '}},
-        {{' ','J',' ',' '},
-         {' ','J',' ',' '},
-         {'J','J',' ',' '},
-         {' ',' ',' ',' '}}
-};
 
-int x = 5, y = 0, b = 0;
+int x = 5, y = 0;
 int speed = 200;
 
 void gotoxy(int x, int y) {
@@ -97,18 +20,35 @@ void gotoxy(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 
+Piece* currentPiece = NULL;
+
+void spawnPiece() {
+    if (currentPiece != NULL) delete currentPiece;
+    int r = rand() % 7;
+    switch(r) {
+        case 0: currentPiece = new PieceI(); break;
+        case 1: currentPiece = new PieceO(); break;
+        case 2: currentPiece = new PieceT(); break;
+        case 3: currentPiece = new PieceS(); break;
+        case 4: currentPiece = new PieceZ(); break;
+        case 5: currentPiece = new PieceL(); break;
+        case 6: currentPiece = new PieceJ(); break;
+    }
+    x = 5; y = 0;
+}
+
 void boardDelBlock() {
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
-            if (blocks[b][i][j] != ' ' && y + j < H)
+            if (currentPiece -> shape[i][j] != ' ' && y + j < H)
                 board[y + i][x + j] = ' ';
 }
 
 void block2Board() {
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
-            if (blocks[b][i][j] != ' ')
-                board[y + i][x + j] = blocks[b][i][j];
+            if (currentPiece -> shape[i][j] != ' ')
+                board[y + i][x + j] = currentPiece -> shape[i][j];
 }
 
 void initBoard() {
@@ -139,7 +79,7 @@ void draw() {
 bool canMove(int dx, int dy) {
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
-            if (blocks[b][i][j] != ' ') {
+            if (currentPiece -> shape[i][j] != ' ') {
                 int tx = x + j + dx;
                 int ty = y + i + dy;
                 if (tx < 1 || tx >= W - 1 || ty >= H - 1) return false;
@@ -148,29 +88,20 @@ bool canMove(int dx, int dy) {
     return true;
 }
 
-bool canRotate(int nextB) {
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            if (blocks[nextB][i][j] != ' ') {
-                int tx = x + j;
-                int ty = y + i;
-                if (tx < 1 || tx >= W - 1 || ty >= H - 1) return false;
-                if (board[ty][tx] != ' ') return false;
-            }
-        }
+bool rotateBlock() {
+    if (currentPiece == nullptr) return false;
+    char backupShape[4][4];
+    int backupState = currentPiece -> state;
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            backupShape[i][j] = currentPiece -> shape[i][j];
+    currentPiece -> rotate();
+    if (!canMove(0, 0)) {
+        currentPiece -> copyShape(backupShape);
+        currentPiece -> state = backupState;
+        return false;
     }
     return true;
-}
-
-int nextRotation(int currentB) {
-    if (currentB >= 0 && currentB <= 1) return (currentB - 0 + 1) % 2 + 0;
-    if (currentB == 2) return 2;
-    if (currentB >= 3 && currentB <= 6) return (currentB - 3 + 1) % 4 + 3;
-    if (currentB >= 7 && currentB <= 8) return (currentB - 7 + 1) % 2 + 7;
-    if (currentB >= 9 && currentB <= 10) return (currentB - 9 + 1) % 2 + 9;
-    if (currentB >= 11 && currentB <= 14) return (currentB - 11 + 1) % 4 + 11;
-    if (currentB >= 15 && currentB <= 18) return (currentB - 15 + 1) % 4 + 15;
-    return currentB;
 }
 
 void removeLine() {
@@ -192,39 +123,34 @@ void removeLine() {
 int main() {
     system("chcp 437");
     srand((unsigned int)time(0));
-    int basicBlocks[] = {0, 2, 3, 7, 9, 11, 15};
-    b = basicBlocks[rand() % 7];
     system("cls");
     initBoard();
-    // Dùng đồng hồ hệ thống để tách biệt input và rơi tự do
+    spawnPiece();
     DWORD lastDropTime = GetTickCount();
-    bool isUpdated = true; // Kiểm tra để vẽ lần đầu tiên
+    bool isUpdated = true;
     while (1) {
         boardDelBlock();
-        // Quét phím
-        if (kbhit()){
+        if (kbhit()) {
             char c = getch();
             if (c == 'a' && canMove(-1,0)) { x--; isUpdated = true; }
             if (c == 'd' && canMove(1,0)) { x++; isUpdated = true; }
             if (c == 's' && canMove(0,1)) {
                 y++;
-                lastDropTime = GetTickCount(); // Khởi động lại mốc rơi
+                lastDropTime = GetTickCount();
                 isUpdated = true;
             }
             if (c == 'w') {
-                int nextB = nextRotation(b);
-                if (canRotate(nextB)) { b = nextB; isUpdated = true; }
+                if (rotateBlock()) isUpdated = true;
             }
             if (c == 'q') break;
         }
-        // Logic rơi tự do theo thời gian thực
+
         if (GetTickCount() - lastDropTime >= (DWORD)speed) {
             if (canMove(0,1)) y++;
             else {
                 block2Board();
                 removeLine();
-                x = 5; y = 0;
-                b = basicBlocks[rand() % 7];
+                spawnPiece();
                 if (!canMove(0, 0)) {
                     block2Board();
                     draw();
@@ -233,15 +159,15 @@ int main() {
                 }
             }
             lastDropTime = GetTickCount();
-            isUpdated = true; // Báo hiệu gạch vừa rớt xuống, cần vẽ lại
+            isUpdated = true;
         }
         block2Board();
-        // Chỉ vẽ lại nếu màn hình có gì đó thay đổi
         if (isUpdated) {
             draw();
             isUpdated = false;
         }
         _sleep(10);
     }
+    delete currentPiece;
     return 0;
 }
