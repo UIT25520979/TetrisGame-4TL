df3ac98add292973824fbd2dd5ca666ababf1665
UIT25521879 <25521879@gm.uit.edu.vn>
2026-05-10 17:02:00 +0700

    PieceS_PieceZ

--- Piece.cpp
+++ Piece.cpp
@@ -16,34 +16,70 @@ void Piece::copyShape(const char source[4][4]) {
             shape[i][j] = source[i][j];
 }
 
-PieceI::PieceI() {
-    char s[4][4] = {
-        {' ','I',' ',' '},
-        {' ','I',' ',' '},
-        {' ','I',' ',' '},
-        {' ','I',' ',' '}
-    };
-    copyShape(s);
-}
-
-void PieceI::rotate() {
-    state = (state + 1) % 2;
-    if (state == 0) {
-        char s[4][4] = {
-            {' ','I',' ',' '},
-            {' ','I',' ',' '},
-            {' ','I',' ',' '},
-            {' ','I',' ',' '}
-        };
-        copyShape(s);
-    }
-    else {
+class PieceS : public Piece {
+public:
+    PieceS() {
         char s[4][4] = {
             {' ',' ',' ',' '},
-            {'I','I','I','I'},
-            {' ',' ',' ',' '},
+            {' ','S','S',' '},
+            {'S','S',' ',' '},
             {' ',' ',' ',' '}
         };
         copyShape(s);
     }
-}
+    void rotate() override {
+        state = (state + 1) % 2;
+        if (state == 0) {
+            char s[4][4] = {
+                {' ',' ',' ',' '},
+                {' ','S','S',' '},
+                {'S','S',' ',' '},
+                {' ',' ',' ',' '}
+            };
+            copyShape(s);
+        }
+        else {
+            char s[4][4] = {
+                {'S',' ',' ',' '},
+                {'S','S',' ',' '},
+                {' ','S',' ',' '},
+                {' ',' ',' ',' '}
+            };
+            copyShape(s);
+        }
+    }
+};
+
+class PieceZ : public Piece {
+public:
+    PieceZ() {
+        char s[4][4] = {
+            {' ',' ',' ',' '},
+            {'Z','Z',' ',' '},
+            {' ','Z','Z',' '},
+            {' ',' ',' ',' '}
+        };
+        copyShape(s);
+    }
+    void rotate() override {
+        state = (state + 1) % 2;
+        if (state == 0) {
+            char s[4][4] = {
+                {' ',' ',' ',' '},
+                {'Z','Z',' ',' '},
+                {' ','Z','Z',' '},
+                {' ',' ',' ',' '}
+            };
+            copyShape(s);
+        }
+        else {
+            char s[4][4] = {
+                {' ',' ','Z',' '},
+                {' ','Z','Z',' '},
+                {' ','Z',' ',' '},
+                {' ',' ',' ',' '}
+            };
+            copyShape(s);
+        }
+    }
+};
