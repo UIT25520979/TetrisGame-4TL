dafb57bd4c3aa73f00211c307faafda47ceab86a
25521626-NguyenTuanTai <25521626@gm.uit.edu.vn>
2026-05-14 21:24:58 +0700

    Định nghĩa cấu trúc kỷ lục và đường dẫn lưu trữ.
    Khai báo cấu trúc dữ liệu bảng xếp hạng và định nghĩa đường dẫn tệp tin lưu trữ kỷ lục.

---
+++ TetrisGame/include/monument.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "common.h"
+#include <vector>
+#include <string>
+
+// =========================================================
+// QUẢN LÝ ETERNAL MONUMENT
+// =========================================================
+
+// Đường dẫn tĩnh tới file dữ liệu
+const std::string MONUMENT_FILE_PATH = "assets/data/highscore.txt";
+
+// Nạp danh sách 5 kỷ lục từ file txt lên game
+std::vector<HighScore> LoadMonumentData();
+
+// Ghi đè danh sách 5 kỷ lục mới xuống file
+void SaveMonumentData(const std::vector<HighScore>& scores);
+
+// Kiểm tra xem điểm số vừa đạt được có lọt top 5 không
+bool IsEligibleForMonument(int newScore);
+
+// Thêm kỷ lục mới
+void AddRecordToMonument(int score, Difficulty level, GameTheme theme);
+
+// Chuyển enum Difficulty thành chuỗi "EASY", "MEDIUM", "HARD" để vẽ lên màn hình
+std::string GetLevelName(Difficulty level);
+
+// Chuyển enum GameTheme thành chuỗi "SPACE", "JUNGLE", "EGYPT" để vẽ lên màn hình
+std::string GetThemeName(GameTheme theme);
